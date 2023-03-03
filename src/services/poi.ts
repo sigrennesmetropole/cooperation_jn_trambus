ab3e6b1abca18736429ccc18384788f4b74cc6ce
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-03-03 15:49:14 +0100

    fix: poi 3D display and undisplay name on hover

--- src/services/poi.ts
+++ src/services/poi.ts
@@ -15,7 +15,7 @@ import {
 } from '@/services/filter'
 import { viewList } from '@/model/views.model'
 import type { View } from '@/model/views.model'
-import { vectorStyleSymbol, StyleItem } from '@vcmap/core'
+import { vectorStyleSymbol, StyleItem, createSync } from '@vcmap/core'
 import { setDistanceDisplayConditionFeature } from '@/services/setDistanceDisplayCondition'
 import { usePoiParkingStore } from '@/stores/poiParking'
 import { useStationsStore } from '@/stores/stations'
@@ -59,31 +59,17 @@ export function displayCurrentPoi(feature: Feature<Geometry>) {
     map3dStore.is3D(),
     false
   )
+  // @ts-ignore
+  feature[createSync] = true
   feature.setStyle(styleItem.style)
-}
-
-/**
- * Normally the name of one poi should be displayed at once
- * It happens that the name of the previous poi are still displayed when displaying the name of a new poi
- * To correct this, we call this function to hide the name of all previous poi and only display the name of the current poi
- */
-export function undisplayPreviousPoiExpectCurrent() {
-  const poiInteractionStore = usePoiInteractionStore()
-  if (poiInteractionStore.previousFeaturesPoi.length === 0) return
-  poiInteractionStore.previousFeaturesPoi.forEach((feature) => {
-    const styleItem = generatePoiStyleWithoutLabel()
-    feature.setStyle(styleItem.style)
-  })
 }
 
 export function undisplayCurrentPoi() {
   const poiInteractionStore = usePoiInteractionStore()
   if (poiInteractionStore.currentFeaturePoi === null) return
-
   const styleItem = generatePoiStyleWithoutLabel()
   poiInteractionStore.currentFeaturePoi.setStyle(styleItem.style)
-  poiInteractionStore.currentFeaturePoi = null
-  undisplayPreviousPoiExpectCurrent()
+  poiInteractionStore.selectCurrentFeaturePoi(null)
 }
 
 async function resetStyleOfPoi(view: View, rennesApp: RennesApp) {
