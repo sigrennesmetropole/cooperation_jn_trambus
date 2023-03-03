1fdb1f3ba40dd7e0c5807c89da4d9aa17907e5e7
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-03-03 16:29:01 +0100

    accessibility home : hide map and label line hover map

--- src/components/ui/UiMap.vue
+++ src/components/ui/UiMap.vue
@@ -13,7 +13,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div ref="mapContainer" class="h-full w-full"></div>
+  <div ref="mapContainer" class="h-full w-full" aria-hidden="true"></div>
 </template>
 <style scoped>
 :deep(.mapElement) {
