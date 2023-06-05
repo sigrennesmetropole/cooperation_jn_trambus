a41da8674d6fc8d38785dcca4ecfa019d338ad77
Yohann Marchat <yohann.marchat@camptocamp.com>
2023-06-05 16:57:27 +0200

    fix: lock zoom out

--- src/components/map/buttons/NavigationButtons.vue
+++ src/components/map/buttons/NavigationButtons.vue
@@ -39,13 +39,14 @@ async function toggle3DMap() {
 async function zoom(out = false, zoomFactor = 2): Promise<void> {
   const activeMap = rennesApp.maps.activeMap
   const viewpoint = await activeMap?.getViewpoint()
+  const maxZoom = rennesApp.get3DMap().getScene()
+    .screenSpaceCameraController.maximumZoomDistance
 
   if (activeMap && viewpoint) {
     let distance = viewpoint.distance / zoomFactor
     if (out) {
-      distance = viewpoint.distance * zoomFactor
+      distance = Math.min(viewpoint.distance * zoomFactor, maxZoom)
     }
-
     const newVp = cloneViewPointAndResetCameraPosition(viewpoint, distance)
     await rennesApp.maps?.activeMap.gotoViewpoint(newVp)
   }
