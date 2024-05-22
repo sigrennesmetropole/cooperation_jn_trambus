5fc92cf12f96e44becd2b89b8d198f4f01900cd0
Alexian Masson <12852383+AlexianMasson@users.noreply.github.com>
2024-05-22 14:40:57 +0200

    Fix: Change photos samples

--- src/model/photos.fixtures.ts
+++ src/model/photos.fixtures.ts
@@ -1,7 +1,18 @@
 import type { PhotoModel } from './photos.model'
-import photo1 from '@/assets/photos/samples/photo1.png'
-import photo2 from '@/assets/photos/samples/photo2.png'
-import photo3 from '@/assets/photos/samples/photo3.png'
+import photo1 from '@/assets/photos/samples/RennesMet_Trambus_T1_01.jpg'
+import photo2 from '@/assets/photos/samples/RennesMet_Trambus_T1_02.jpg'
+import photo3 from '@/assets/photos/samples/RennesMet_Trambus_T1_03.jpg'
+import photo4 from '@/assets/photos/samples/RennesMet_Trambus_T1_04.jpg'
+import photo5 from '@/assets/photos/samples/RennesMet_Trambus_T1_05.jpg'
+import photo6a from '@/assets/photos/samples/RennesMet_Trambus_T1_06A.jpg'
+import photo6b from '@/assets/photos/samples/RennesMet_Trambus_T1_06B.jpg'
+import photo7 from '@/assets/photos/samples/RennesMet_Trambus_T1_07.jpg'
+import photo8 from '@/assets/photos/samples/RennesMet_Trambus_T2_08.jpg'
+import photo9 from '@/assets/photos/samples/RennesMet_Trambus_T2_09.jpg'
+import photo10 from '@/assets/photos/samples/RennesMet_Trambus_T2_10.jpg'
+import photo11 from '@/assets/photos/samples/RennesMet_Trambus_T2_11.jpg'
+import photo12 from '@/assets/photos/samples/RennesMet_Trambus_T2_12.jpg'
+import photo13 from '@/assets/photos/samples/RennesMet_Trambus_T2_14.jpg'
 
 export const photoFixtures = (): PhotoModel[] => [
   {
@@ -10,10 +21,54 @@ export const photoFixtures = (): PhotoModel[] => [
   },
   {
     url: photo2,
-    line: 2,
+    line: 1,
   },
   {
     url: photo3,
-    line: 3,
+    line: 1,
+  },
+  {
+    url: photo4,
+    line: 1,
+  },
+  {
+    url: photo5,
+    line: 1,
+  },
+  {
+    url: photo6a,
+    line: 1,
+  },
+  {
+    url: photo6b,
+    line: 1,
+  },
+  {
+    url: photo7,
+    line: 1,
+  },
+  {
+    url: photo8,
+    line: 2,
+  },
+  {
+    url: photo9,
+    line: 2,
+  },
+  {
+    url: photo10,
+    line: 2,
+  },
+  {
+    url: photo11,
+    line: 2,
+  },
+  {
+    url: photo12,
+    line: 2,
+  },
+  {
+    url: photo13,
+    line: 2,
   },
 ]
