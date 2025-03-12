08977eac83b47ece76f5cfc322c64dfdbaf57503
Alexian Masson <12852383+AlexianMasson@users.noreply.github.com>
2025-03-12 14:36:08 +0100

    feat: add photos as fixtures

--- src/model/photos.fixtures.ts
+++ src/model/photos.fixtures.ts
@@ -13,6 +13,24 @@ import photo10 from '@/assets/photos/samples/RennesMet_Trambus_T2_10.jpg'
 import photo11 from '@/assets/photos/samples/RennesMet_Trambus_T2_11.jpg'
 import photo12 from '@/assets/photos/samples/RennesMet_Trambus_T2_12.jpg'
 import photo13 from '@/assets/photos/samples/RennesMet_Trambus_T2_14.jpg'
+import photo14 from '@/assets/photos/samples/Trajectoires_Trambus_T3_A.Bonnin.jpg'
+import photo15 from '@/assets/photos/samples/Trajectoires_Trambus_T3_BdVerdun.jpg'
+import photo16 from '@/assets/photos/samples/Trajectoires_Trambus_T3_GaldeGaulle.jpg'
+import photo17 from '@/assets/photos/samples/Trajectoires_Trambus_T3_LattreTassigny.jpg'
+import photo18 from '@/assets/photos/samples/Trajectoires_Trambus_T3_PontBoeuf.jpg'
+import photo19 from '@/assets/photos/samples/Trajectoires_Trambus_T3_PorteLoges.jpg'
+import photo20 from '@/assets/photos/samples/Trajectoires_Trambus_T3_RueChateaugiron.jpg'
+import photo21 from '@/assets/photos/samples/Trajectoires_Trambus_T3_StHelier.jpg'
+import photo22 from '@/assets/photos/samples/Trajectoires_Trambus_T3_Uttenreuth.jpg'
+import photo23 from '@/assets/photos/samples/Trajectoires_Trambus_T4_AvJan_Courte.jpg'
+import photo24 from '@/assets/photos/samples/Trajectoires_Trambus_T4_AvSchuman.jpg'
+import photo25 from '@/assets/photos/samples/Trajectoires_Trambus_T4_BruzCentre_Longue.jpg'
+import photo26 from '@/assets/photos/samples/Trajectoires_Trambus_T4_Europe.jpg'
+import photo27 from '@/assets/photos/samples/Trajectoires_Trambus_T4_HaieGautrais.jpg'
+import photo28 from '@/assets/photos/samples/Trajectoires_Trambus_T4_IleMoines.jpg'
+import photo29 from '@/assets/photos/samples/Trajectoires_Trambus_T4_JVallès.jpg'
+import photo30 from '@/assets/photos/samples/Trajectoires_Trambus_T4_RDodin.jpg'
+import photo31 from '@/assets/photos/samples/Trajectoires_Trambus_T4_SecteurAeroport.jpg'
 
 export const photoFixtures = (): PhotoModel[] => [
   {
@@ -70,5 +88,77 @@ export const photoFixtures = (): PhotoModel[] => [
   {
     url: photo13,
     line: 2,
+  },
+  {
+    url: photo14,
+    line: 3,
+  },
+  {
+    url: photo15,
+    line: 3,
+  },
+  {
+    url: photo16,
+    line: 3,
+  },
+  {
+    url: photo17,
+    line: 3,
+  },
+  {
+    url: photo18,
+    line: 3,
+  },
+  {
+    url: photo19,
+    line: 3,
+  },
+  {
+    url: photo20,
+    line: 3,
+  },
+  {
+    url: photo21,
+    line: 3,
+  },
+  {
+    url: photo22,
+    line: 3,
+  },
+  {
+    url: photo23,
+    line: 4,
+  },
+  {
+    url: photo24,
+    line: 4,
+  },
+  {
+    url: photo25,
+    line: 4,
+  },
+  {
+    url: photo26,
+    line: 4,
+  },
+  {
+    url: photo27,
+    line: 4,
+  },
+  {
+    url: photo28,
+    line: 4,
+  },
+  {
+    url: photo29,
+    line: 4,
+  },
+  {
+    url: photo30,
+    line: 4,
+  },
+  {
+    url: photo31,
+    line: 4,
   },
 ]
